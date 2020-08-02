2b80c0aa23c695d9c2b3c2b03c50d511baf8983c
bitmapper <bitmapper85@gmail.com>
2020-08-02 19:10:16 -0300

    fixes

--- build-sbcl.sh
+++ build-sbcl.sh
@@ -1,2 +1,3 @@
 find . -name '*.fasl' | xargs rm
-Y2=$PWD HASKELL=$PWD PRELUDE=$PWD/progs/prelude PRELUDEBIN=$PWD/progs/prelude/sbcl sbcl --eval "(progn (if (not (find-package \"MUMBLE-IMPLEMENTATION\")) (make-package \"MUMBLE-IMPLEMENTATION\" :use '(\"COMMON-LISP\")) (in-package \"MUMBLE-IMPLEMENTATION\")))" --load com/mcl/mcl-setup.lisp --load cl-support/cl-init.lisp --load com/mcl/savesys.lisp
+Y2=$PWD HASKELL=$PWD PRELUDE=$PWD/progs/prelude PRELUDEBIN=$PWD/progs/prelude/sbcl sbcl --eval "(progn (if (not (find-package \"MUMBLE-IMPLEMENTATION\")) (make-package \"MUMBLE-IMPLEMENTATION\" :use '(\"COMMON-LISP\")) (in-package \"MUMBLE-IMPLEMENTATION\")))" --load com/sbcl/sbcl-setup.lisp --load cl-support/cl-init.lisp
+# --load com/sbcl/savesys.lisp
